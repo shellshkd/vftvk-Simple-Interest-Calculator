8d0251fa426082489bc27dca692d5d9534db5bdc
shellshkd <94668702+shellshkd@users.noreply.github.com>
2021-12-22 19:24:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+    
     
 };
 
@@ -13,4 +16,12 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+var resultDisplay = document.getElementById("result");
+resultDisplay.innerHTML = "If you deposit  " 
+    + "<span class='highlight'>" + principal + "</span>."  
+    + ", <br> at an interest rate of "+ "<span class='highlight'>" 
+    + rate + "</span>%." + "<br> You will receive an amount of " 
+    + "<span class='highlight'>" + interest + "</span>" 
+    + ", <br>in the year " + "<span class='highlight'>" 
+    + year + "</span>"; 
         
