95f21938c090b363b7fd5a7bff636a5bb4cced6b
Michelle Williams <shellshkd18@gmail.com>
2021-12-24 06:34:17 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    
+    //below creates alert when users doesn't enter positive
+    if(principal <= 0 || ""){
+       alert("Enter a number greater than 0");
+       document.getElementById("principal").focus(); 
+    } 
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
@@ -10,14 +15,12 @@ function compute()
      resultDisplay.innerHTML = "If you deposit  " 
     + "<span>" + principal + "</span>."  
     + ", <br> at an interest rate of "+ "<span>" 
-    + rate + "</span> %" + "<br> You will receive an amount of " 
-    + "<span>" + interest + "</span>" 
-     <br> in the year " + "<span>" 
+    + rate + "</span> %." + "<br> You will receive an amount of " 
+    + "<span>" + interest + "</span>" +
+    " , <br> in the year " + "<span>" 
     + year + "</span>"; 
         
-    
-    
-};
+    };
 
 function updateRate() 
 {
